98d4f8e72028eae84c9a763d92bf04010a61b74c
xulun <ziying.liuziying@alibaba-inc.com>
2020-09-14 20:52:12 +0800

    update

--- dqn.py
+++ dqn.py
@@ -7,6 +7,9 @@ from stable_baselines3.dqn import MlpPolicy
 #env = gym.make('Breakout-v0')
 env = gym.make('SpaceInvaders-v0')
 
+print(env.action_space)
+print(env.get_action_meanings())
+
 model = DQN(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=100000, log_interval=4)
 model.save("dqn_space_invaders")
